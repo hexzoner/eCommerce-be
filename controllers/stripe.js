343865c5b3437a6bffa4eae23526e4e9c48ecf44
hexzoner <hexzon@gmail.com>
2024-11-28 14:46:06 +0100

    stripe checkout polishing

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -24,3 +24,23 @@ export const createCheckout = async (req, res) => {
   // res.redirect(303, session.url);
   res.status(200).json({ url: session.url, sessionId: session.id });
 };
+
+export const verifyCheckoutSession = async (req, res) => {
+  const { sessionId } = req.params;
+  if (!sessionId) return res.status(400).json({ message: "No session id provided" });
+
+  const stripeSession = stripe(process.env.STRIPE_SECRET_KEY);
+  const session = await stripeSession.checkout.sessions.retrieve(sessionId);
+  res.status(200).json({
+    id: session.id,
+    paymentStatus: session.payment_status,
+    paymentMethod: session.payment_method,
+    paymentType: session.payment_type,
+    paymentStatusTransitions: session.payment_status_transitions,
+    currency: session.currency,
+    customerDetalis: session.customer_details,
+    createdDate: session.created,
+    amountTotal: session.amount_total,
+    totalDetails: session.total_details,
+  });
+};
--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { createCheckout } from "../controllers/stripe.js";
+import { createCheckout, verifyCheckoutSession } from "../controllers/stripe.js";
 import { asyncHandler } from "../middleware/errorHandler.js";
 
 const checkoutRouter = Router();
 
 checkoutRouter.route("/").post(asyncHandler(createCheckout));
+checkoutRouter.route("/:sessionId").get(asyncHandler(verifyCheckoutSession));
+
+export default checkoutRouter;
